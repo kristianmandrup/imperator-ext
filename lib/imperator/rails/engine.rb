353edfc1c017e892f29d0057ce64bc7028a2dc9b
Kristian Mandrup <kmandrup@gmail.com>
2013-03-22 15:59:41 +0100

    fix autoload_paths engine bug

--- lib/imperator/rails/engine.rb
+++ lib/imperator/rails/engine.rb
@@ -2,7 +2,7 @@ module Imperator
   module Rails
     class Engine < ::Rails::Engine
       initializer 'Imperator ext setup' do
-        config.auto_load_paths += Dir[Rails.root.join('app/commands')]
+        config.autoload_paths += Dir[Rails.root.join('app/commands')]
       end
     end
   end
